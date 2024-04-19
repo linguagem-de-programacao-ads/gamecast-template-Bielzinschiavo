0c561de92d425eb32064a8ad86dfc8c0937a7136
Gabriel <gabriel.soliveira@sptech.school>
2024-04-18 21:18:11 -0300

    BootCamp Aula 3 - Informações passadas para o front e responsividade css

--- js/buscar.js
+++ js/buscar.js
@@ -1,6 +1,4 @@
 async function buscar() {
-    console.log("Passei por aqui");
-
     const resposta = await fetch("https://660f44b0356b87a55c510d97.mockapi.io/agendas");
     const respostaDadosAgenda = await resposta.json();
 
@@ -9,10 +7,32 @@ async function buscar() {
     const cards = document.getElementById("cards_games")
 
    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-         return "TESTE "
-    });
+         return `
+         <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome}</strong> </p>
+                    <p>${itemAgenda.descricao}</strong></p>
+
+                    <div class="gamers">
+                    ${
+                        itemAgenda.gamers.map((gamerItem) => {
+                            return `<div class="gamerItem">@${gamerItem}</div>`
+                        }).join('')
+                    }
+                    </div>
+
+                  
+                </div>  
+                <div class="assistir">
+                        <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+         `
+    }).join('');
 }
 
-console.log("antes de buscar")
 buscar();
-console.log("depois de buscar")
